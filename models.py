f5e2d3106f022369fef16e0b941a8f2d046facef
Sala 3 <sala3@FI-LabSistemas.ucatolica.loc>
2025-12-05 08:28:15 -0500

    Creacion de la clase base del jugador, danny ve creando a los equipos

--- models.py
+++ models.py
@@ -1,1 +1,17 @@
-## Yo creo los modelos de jugador tu crea los del equipo 
+from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional
+
+
+class jugadorBase(SQLModel):
+    name: str | None = Field(description= "Jugador name")
+    Ncamisa: int | None = Field(description = "Numero de camisa" )
+    Fecha: int | None = Field(description= "Fecha de nacimiento")
+    estado: str | None = Field(description= "Estado del jugador")
+    alive : bool | None = Field(description = "True = active, False= deleted", default = True)
+    img: Optional[str] = Field(default = None, description="Jugador Image")
+
+class SpiderMan(JugadorBase, table=True):
+    id: int | None = Field(default = None, primary_key=True)
+    universe_id:int =Field(foreign_key = "universe.id")
+    Universe: universe = Relationship(back_populates="spiderMans")
